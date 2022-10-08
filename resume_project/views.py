4d4e5ca861697e6754732fad3e34239b3d34885c
premsagar2004 <72159239+premsagar2004@users.noreply.github.com>
2022-10-08 22:56:42 +0530

    Updating views libraries

--- resume_project/views.py
+++ resume_project/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
 
 def home(request):
     return render(request, 'resume_data/preview1.html')
